0f13af45e2a097e9cf8fde947feaa9c98abcde20
Yanayfi24-meet <109971369+yanayfinkelstein@users.noreply.github.com>
2023-07-17 18:16:45 +0300

    :)

--- pyReview.py
+++ pyReview.py
@@ -1,6 +1,7 @@
+print("fuck you")
 
 def create_youtube_video(description, title):
-	video = {"title": title, "description": description, "likes": 0, "dislikes":0, "comments":comments}
+	video = {"title": title, "description": description, "likes": 0, "dislikes":0, "comments":{}}
 	return video
 def like(dictionary):
 	if "likes" in dictionary:
@@ -13,7 +14,8 @@ def dislike(dictionary):
 		dictionary["dislikes"] = dictionary["dislikes"] + 1
 		return dictionary
 def add_comment(video, username, comment_text):
-		comments = {username: comment_text;}
+		comments = {username: comment_text}
+		video["comments"][username]=comment_text
 		return comments
 
 
@@ -24,7 +26,3 @@ for i in range(6):
 	like(video)
 dislike(video)
 print(video)
-	
-
-		
-
